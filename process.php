31c383af43e54f2b614fb95e571710b7d6978fe4
kjvfghkrgs <fpubvn@gmail.com>
2019-03-17 15:03:03 +0700

    Update and rename README.md to process.php

---
+++ process.php
@@ -0,0 +1,13 @@
+@$filename = $_GET['url'];
+header('Content-Type: video/MP2T');
+header('Content-Disposition: attachment; filename="' . md5($filename) . '.ts"');
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$filename);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 500);
+curl_setopt($ch, CURLOPT_WRITEFUNCTION, function($curl, $data) {
+    echo $data;
+    return strlen($data);
+});
+curl_exec($ch);
+curl_close($ch);
